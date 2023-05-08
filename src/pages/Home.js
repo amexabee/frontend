27b268f23e9bf0d264d4575e914ee23dbbc0fc65
Amanuel Galema <amexabee@yahoo.com>
2023-05-08 17:37:06 +0300

    Add workout fetching and creating functionality

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,31 @@
+import { useEffect, useState } from 'react';
+import WorkoutDetails from '../components/WorkoutDetails';
+import WorkoutForm from '../components/WorkoutForm';
+
 const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const res = await fetch('http://localhost:4000/api/workouts');
+      const json = await res.json();
+
+      if (res.ok) {
+        setWorkouts(json);
+      }
+    };
+
+    fetchWorkouts();
+  }, []);
   return (
     <div className="home">
-      <h2>Home</h2>
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
+      </div>
+      <WorkoutForm />
     </div>
   );
 };
