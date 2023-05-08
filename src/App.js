9a382debb2d6538eb5046632ab4f05ea4f3bd5c0
Amanuel Galema <amexabee@yahoo.com>
2023-05-08 14:13:31 +0300

    Add Navbar, add some styles

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 // pages and components
 import Home from './pages/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
   );
 }
 
